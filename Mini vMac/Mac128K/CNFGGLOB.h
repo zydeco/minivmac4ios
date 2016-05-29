0b8c97d458c98677585d2ca4f4d3bcc1848b5608
Jesús A. Álvarez <zydeco@namedfork.net>
2016-05-29 14:12:14 +0200

    add Mac 128K bundle

---
+++ Mini vMac/Mac128K/CNFGGLOB.h
@@ -0,0 +1,19 @@
+#define RomFileName "Mac128K.ROM"
+
+#define WantInitSpeedValue 2
+#define WantInitNotAutoSlow 0
+#define EnableAutoSlow 1
+
+#define MySoundEnabled 1
+#define MySoundRecenterSilence 0
+#define kLn2SoundSampSz 3
+
+#define NumDrives 2
+
+#define vMacScreenHeight 342
+#define vMacScreenWidth 512
+#define vMacScreenDepth 0
+
+#define kROM_Size 0x00010000
+
+#include "../CNFGGLOB.h"
