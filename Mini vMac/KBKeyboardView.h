c548061f4267b0a4d9ac78ec10cf2afce39d94e5
Jesús A. Álvarez <zydeco@namedfork.net>
2020-09-30 22:49:59 +0200

    add missing nullability annotation

--- Mini vMac/KBKeyboardView.h
+++ Mini vMac/KBKeyboardView.h
@@ -26,5 +26,5 @@
 @property (nonatomic, readonly, nonnull) NSArray<KBKey*>* keys;
 @property (nonatomic, readonly, nonnull) NSArray<KBKey*>* stickyKeys;
 
-- (instancetype)initWithFrame:(CGRect)frame safeAreaInsets:(UIEdgeInsets)safeAreaInsets;
+- (instancetype _Nonnull)initWithFrame:(CGRect)frame safeAreaInsets:(UIEdgeInsets)safeAreaInsets;
 @end
