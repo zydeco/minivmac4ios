ada4e0a4ddd5f9910a8a212d56deb935a52bd901
Jesús A. Álvarez <zydeco@namedfork.net>
2022-05-26 16:17:37 +0200

    update to Mini vMac 37.03 beta

--- Mini vMac/CNFUIALL.h
+++ Mini vMac/CNFUIALL.h
@@ -67,6 +67,6 @@ typedef uint64_t ui6b;
 #define NeedIntlChars 1
 #define kStrAppName "Mini vMac"
 #define kAppVariationStr "minivmac-b37-ios"
-#define kStrCopyrightYear "2020"
+#define kStrCopyrightYear "2022"
 #define kMaintainerName "Jesús A. Álvarez"
 #define kStrHomePage "https://namedfork.net/minivmac"
